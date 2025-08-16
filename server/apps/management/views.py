de4e3217fe8bfea6998e13b276ed07ebc501f1a1
haui <suihuai@bank.com.tw>
2025-08-16 14:59:00 +0800

    [create] Day2-07_depend on different api method to return respective message

--- server/apps/management/views.py
+++ server/apps/management/views.py
@@ -2,6 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])  # 限定打api的方法
 def hello(request):  # django 裡面的view都要接request這個參數
-    return Response({"message": "Hello~123"})
+    if request.method == "GET":
+        message = "GET success"
+    elif request.method == "POST":
+        message = "POST success"
+
+    return Response({"message": message})
