9b8370c5c0d38c50f59dc210cf2ad7488b88f15f
haui <suihuai@bank.com.tw>
2025-08-16 15:10:56 +0800

    [create] Day2-08_change define to class to control api

--- server/apps/management/views.py
+++ server/apps/management/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 @api_view(["GET", "POST"])  # 限定打api的方法
@@ -10,3 +11,17 @@ def hello(request):  # django 裡面的view都要接request這個參數
         message = "POST success"
 
     return Response({"message": message})
+
+
+# api寫成class的好處是可以制定自己的method
+class HiView(APIView):
+    # 有self 是因爲出現在class當中 #但因為是api所以要有request參數
+
+    def _build_message(self, method):
+        return f"Hi with {method} Method"
+
+    def get(self, request):
+        return Response({"message": self._build_message("GET")})
+
+    def post(self, request):
+        return Response({"message": self._build_message("POST")})
--- server/urls.py
+++ server/urls.py
@@ -18,10 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from server.apps.management.views import hello
+from server.apps.management.views import HiView, hello
 
 urlpatterns = [
     # 指定使用者呼叫的位置： path("path",view位置)
     path("admin/", admin.site.urls),
     path("api/v1/practice/hello", hello),
+    path(
+        "api/v1/practice/hi", HiView.as_view()
+    ),  # 預設承接的是一個方法，所以如果是class型態的就要用as_view()轉為方法
 ]
