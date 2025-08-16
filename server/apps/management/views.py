c22769c00c07f51cfc9cc2c95fcc24b9a4437001
haui <suihuai@bank.com.tw>
2025-08-16 14:44:55 +0800

    [create] Day2-06_adjust api response

--- server/apps/management/views.py
+++ server/apps/management/views.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 
 @api_view(["GET"])
 def hello(request):  # django 裡面的view都要接request這個參數
-    return Response({"message:Hello~123"})
+    return Response({"message": "Hello~123"})
