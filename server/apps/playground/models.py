187e84024faa959910881c8f194722fa931b94be
huai <suihuai@bank.com.tw>
2025-08-30 10:06:53 +0800

    Day3-02_create playground item table in database

--- server/apps/playground/models.py
+++ server/apps/playground/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# jango 會自動產生一個id 所以不用特別打
+
+class Item(models.Model):
+    name  =models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=False)
