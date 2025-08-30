9d70a1eb27ee22a6d7bd6663f04a8d24b041424d
huai <suihuai@bank.com.tw>
2025-08-30 11:03:50 +0800

    Day3-03_create dev virtual environment

--- server/apps/playground/admin.py
+++ server/apps/playground/admin.py
@@ -6,4 +6,4 @@ from server.apps.playground.models import Item
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ("name","is_active")
-    list_fliter = ("is_active",)
+    list_fliter = ("is_active",) #這個,要記得加 不然執行不會過
--- server/apps/playground/models.py
+++ server/apps/playground/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # jango 會自動產生一個id 所以不用特別打
 
 class Item(models.Model):
-    name  =models.CharField(max_length=255)
+    name  = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     is_active = models.BooleanField(default=False)
